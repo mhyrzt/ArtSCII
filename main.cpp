ed68d6488e4926ca17c93f10a072186c4ddee152
Mahyar Riazati <mr.riazati1999@gmail.com>
2021-02-09 23:58:07 +0330

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define STB_IMAGE_IMPLENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLENTATION
+#include "stb_image_write.h"
+
+int main(){
+
+}
